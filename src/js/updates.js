da08151253eb7639121480e5d176a8d7179837e0
Maurício Batista <eddloschi@gmail.com>
2020-04-21 04:48:38 -0300

    Added detailed information on the updates section

--- src/js/updates.js
+++ src/js/updates.js
@@ -6,9 +6,12 @@ const updatesList = document.getElementById("updates-list")
 data.forEach(entry => {
   let listItem = document.createElement("li")
   let anchor = document.createElement("a")
-  let date = document.createTextNode(DateTime.fromISO(entry.date).toLocaleString(DateTime.DATE_SHORT))
+  let date = document.createTextNode(`${DateTime.fromISO(entry.date).toLocaleString(DateTime.DATE_SHORT)}:`)
+  let stats = document.createTextNode(` Suspeitos: ${entry.suspects} - Confirmados: ${entry.confirmed} - Descartados: ${entry.discarded}`)
   anchor.href = entry.link
   anchor.appendChild(date)
   listItem.appendChild(anchor)
+  listItem.appendChild(document.createElement("br"))
+  listItem.appendChild(stats)
   updatesList.appendChild(listItem)
 })
